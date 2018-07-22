db27adb8a5895aa72027d052326ecd7dc5308723
prenticedavid <david.prentice@farming.co.uk>
2018-07-22 17:04:22 +0100

    speed up ESP32 PIN_LOW/HIGH.  improve casting later

--- utility/mcufriend_shield.h
+++ utility/mcufriend_shield.h
@@ -676,8 +676,10 @@ static void setReadDir()
 #define READ_8(dst)   { RD_STROBE; READ_DELAY; dst = read_8(); RD_IDLE; }
 #define READ_16(dst)  { uint8_t hi; READ_8(hi); READ_8(dst); dst |= (hi << 8); }
 
-#define PIN_LOW(p, b)        (digitalWrite(b, LOW))
-#define PIN_HIGH(p, b)       (digitalWrite(b, HIGH))
+#define PIN_LOW(p, b)        (*(&p+2)) = (1<<(b&31))
+#define PIN_HIGH(p, b)        (*(&p+1)) = (1<<(b&31))
+//#define PIN_LOW(p, b)        (digitalWrite(b, LOW))
+//#define PIN_HIGH(p, b)       (digitalWrite(b, HIGH))
 #define PIN_OUTPUT(p, b)     (pinMode(b, OUTPUT))
 
 #else
